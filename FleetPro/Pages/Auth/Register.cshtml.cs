c7948fe57db567045aa84918d646d858225604bc
santo1827 <136288759+santo1827@users.noreply.github.com>
2025-10-10 14:08:12 +1000

    New Unified Register Page

--- FleetPro/Pages/Auth/Register.cshtml.cs
+++ FleetPro/Pages/Auth/Register.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace InterportCargo.Web.Pages.Auth;
 
-public class RegisterCustomerModel : PageModel
+public class RegisterModel : PageModel
 {
     public void OnGet() {}
 }
